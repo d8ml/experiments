f99086e7dbd3fb5594b71b19ad14b283efe620cc
Petr Ivanov <d8ml@yandex.ru>
2024-04-08 11:26:52 +0500

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 start=$(date +"%s")
 
 ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+
+docker-compose down && docker-compose build --pull && docker-compose up -d
 sudo docker pull d8ml/forest_of_habits
+
+echo "next step 1"
 
 #CONTAINER_NAME=forest_of_habits
 #if [ "$(sudo docker ps -qa -f name=$CONTAINER_NAME)" ]; then
@@ -16,7 +20,9 @@ sudo docker pull d8ml/forest_of_habits
 cd docker/
 sudo docker compose up -d
 
-exit
+echo "next step 2"
+
+#exit
 ENDSSH
 
 if [ $? -eq 0 ]; then
